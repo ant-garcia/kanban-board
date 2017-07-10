cc26c0dbda4605323f2923980b2dd2bbd64b5b8e
ant-garcia <anthonyg576@gmail.com>
2017-07-10 19:57:10 -0400

    switched to JPA models

--- app/models/User.java
+++ app/models/User.java
@@ -1,9 +1,25 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user", schema = "kanban")
 public class User{
-	private String mName;
+
+	@Id
+	@Column(name = "email")
 	private String mEmail;
+
+	@Column(name = "name")
+	private String mName;
+
+	@Column(name = "password")
 	private String mPassword;
+
+	public User(){}
 
 	public User(String name, String email, String password){
 		mName = name;
@@ -15,11 +31,23 @@ public class User{
 		return mName;
 	}
 
+	public void setName(String name){
+		mName = name;
+	}
+
 	public String getEmail(){
 		return mEmail;
 	}
 
+	public void setEmail(String email){
+		mEmail = email;
+	}
+
 	public String getPassword(){
 		return mPassword;
+	}
+
+	public void setPassword(String password){
+		mPassword = password;
 	}
 }
--- app/models/Widget.java
+++ app/models/Widget.java
@@ -1,24 +1,43 @@
 package models;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "widget", schema = "kanban")
 public class Widget{
+
+	@Id
+	@Column(name = "id")
 	private int mId;
+
+	@Column(name = "name")
 	private String mName;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "status")
 	private String mStatus;
+
+	@Column(name = "description")
 	private String mDescription;
 
-	public Widget(int id, String name, String description){
+	public Widget(){}
+
+	public Widget(int id, String email, String name, String description){
 		mId = id;
+		this.email = email;
 		mName = name;
 		mStatus = "TODO";
 		mDescription = description;
 	}
 
-	public Widget(int id, String name, String status, String description){
+	public Widget(int id, String email, String name, String status, String description){
 		mId = id;
+		this.email = email;
 		mName = name;
 		mStatus = status;
 		mDescription = description;	
@@ -27,21 +46,41 @@ public class Widget{
 	public int getId(){
 		return mId;
 	}
+
+	public void setId(int id){
+		mId = id;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
 	
 	public String getName(){
 		return mName;
+	}
+
+	public void setName(String name){
+		mName = name;
 	}
 
 	public String getStatus(){
 		return mStatus;
 	}
 
+	public void setStatus(String status){
+		mStatus = status;
+	}
+
 	public String getDescription(){
 		return mDescription;
 	}
 
-	public void setStatus(String status){
-		mStatus = status;
+	public void setDescription(String description){
+		mDescription = description;
 	}
 
 	public String toString(){
